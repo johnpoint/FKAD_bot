32a09a6cb9709c0fe4a07d05026be9062c9dc533
johnpoint <me@lvcshu.com>
2022-03-13 16:57:14 +0800

    config: add custom tls cert

--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ var Config = &ServiceConfig{}
 
 type ServiceConfig struct {
 	configPath       string
+	ServiceName      string             `yaml:"service_name"`
 	HttpServerListen string             `yaml:"http_server_listen"`
 	Environment      string             `yaml:"environment"`
 	MongoDBConfig    *MongoDBConfig     `yaml:"mongo_db_config"`
@@ -23,6 +24,8 @@ type TelegramBotConfig struct {
 	Listen           string `yaml:"listen"`
 	VerifyPageUrl    string `yaml:"verify_page_url"`
 	VerifyPageSecret string `yaml:"verify_page_secret"`
+	CustomTLSCert    string `yaml:"custom_tls_cert"`
+	CustomTLSKey     string `yaml:"custom_tls_key"`
 }
 
 func (c *ServiceConfig) SetPath(path string) *ServiceConfig {
